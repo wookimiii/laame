752c1a781ca16e2c3d272d1665473a035cc5540b
andrew kim <akim@attinteractive.com>
2010-12-18 12:18:49 -0800

    raising logs to debug level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ Blogcast::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
-  config.log_level = :error
+   config.log_level = :debug
+  #config.log_level = :error
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
